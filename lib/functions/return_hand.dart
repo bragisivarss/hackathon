dec26301ba1f603e0ce43d849a9e1b9807a0ef5e
bragisivarss <sigursteinnbragi@gmail.com>
2023-10-21 12:10:31 +0000

    return hand

--- lib/functions/return_hand.dart
+++ lib/functions/return_hand.dart
@@ -13,10 +13,14 @@ print(hand); // Should print []
 
 
 void ReturnHand(List<int> hand, List<int> deck){
+  deck.addAll(hand);
+  hand.clear();
 
 }
 
 void ReturnHands(List<int> playerHand, List<int> houseHand, List<int> deck){
+  ReturnHand(playerHand, deck);
+  ReturnHand(houseHand, deck);
 
 }
 /*
